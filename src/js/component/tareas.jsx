280d59185f6250c2d0e0c111ddc5228eeac97baf
RodriMaldonadoMedina <maldonadomedinarodrigo@gmail.com>
2022-08-19 18:20:19 +0000

    se arreglo el mostrar la cantidad de tareas, el numero al final de la lista

--- src/js/component/tareas.jsx
+++ src/js/component/tareas.jsx
@@ -1,64 +1,59 @@
 import React, { useState } from "react";
-import Tarea from "./tarea.jsx"
+import Tarea from "./tarea.jsx";
 
+const Tareas = () => {
+  const [nombreTarea, setNombreTarea] = useState([]);
 
-const Tareas = ()=>{
+  const agregarTarea = (e) => {
+    if (e.target.value.trim() && e.key === "Enter") {
+      //setNombreTarea((prev)=>prev.concat(e.target.value))
+      setNombreTarea((prev) => [...prev, e.target.value]);
+      //lo necesito para limpiar el input por la asincronia
+      setTimeout(() => {
+        e.target.value = "";
+      }, 500);
+    }
+  };
 
-    const [nombreTarea, setNombreTarea] = useState([]);
+  const eliminarTarea = (nombre) => {
+    //nombre es el evento del onClick, por eso necesito hacer todo ese camino
+    setNombreTarea(nombreTarea.filter((element) => element !== nombre));
+  };
 
-    const agregarTarea = (e) => {
-        if (e.target.value !== "" && e.key === "Enter") {
-        //setNombreTarea((prev)=>prev.concat(e.target.value))
-        setNombreTarea((prev) => [...prev, e.target.value]);
-        //lo necesito para limpiar el input por la asincronia
-        setTimeout(() => {
-            e.target.value = "";
-        }, 500);
-        }
-    };
-
-    const eliminarTarea = (nombre) => {
-        //nombre es el evento del onClick, por eso necesito hacer todo ese camino
-        let tareaFiltrada = nombreTarea.filter(
-        (element) =>element !== nombre);
-        setNombreTarea(tareaFiltrada);
-    };
-
-    console.log(nombreTarea);
-    return (
-
-        /*
-        fetch('https://assets.breatheco.de/apis/fake/todos/user/alesanchezr', {
-            method: "GET",
-            //body: JSON.stringify(todos),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-        .then(resp => {
-            if (resp.ok >= 200 && resp.ok<300){
-                console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
-                console.log(resp.status); // el código de estado = 200 o código = 400 etc.
-                console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
-            }
-            else console.log("Hay un error en el request")
-            return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+  function functionFetch() {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/alesanchezr", {
+      method: "PUT",
+      body: JSON.stringify(nombreTarea),
+      headers: {
+        "Content-Type": "application/json",
+      },
     })
-    .then(data => {
+      .then((resp) => {
+        console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+        console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+        console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
+        console.log("Hay un error en el request");
+        return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+      })
+      .then((data) => {
         //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
         console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
-    })
-    .catch(error => {
+      })
+      .catch((error) => {
         //manejo de errores
         console.log(error);
-    }));
+      });
+  }
 
-*/
-    
-
+  return (
     <div>
       <div className="row miInput my-3">
-        <input type="text" onKeyDown={agregarTarea} className="col-12" placeholder="Ingrese la tarea a realizar"/>
+        <input
+          type="text"
+          onKeyDown={agregarTarea}
+          className="col-12"
+          placeholder="Ingrese la tarea a realizar"
+        />
       </div>
 
       <ul className="list-group list-group-flush">
@@ -72,9 +67,12 @@ const Tareas = ()=>{
           />
         ))}
         <li className="list-group-item text-center">
-            {(nombreTarea === 0) ? (`No hay Tareas`) : (`cantidad de tareas de hoy : ${nombreTarea.length}`)}
+          {nombreTarea.length === 0
+            ? `No hay Tareas`
+            : `cantidad de tareas de hoy : ${nombreTarea.length}`}
         </li>
       </ul>
+      {functionFetch()}
     </div>
   );
 };

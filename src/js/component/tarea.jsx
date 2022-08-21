674cb471a817526e1a8180e82a75381745be3774
RodriMaldonadoMedina <maldonadomedinarodrigo@gmail.com>
2022-08-21 15:43:27 +0000

    Se arreglo el contador de las tareas pendientes

--- src/js/component/tarea.jsx
+++ src/js/component/tarea.jsx
@@ -5,7 +5,6 @@ const Tarea = ({nombre, clase, eliminarTarea}) => {
     return (
         <div>
             <li className={clase}>
-                {console.log(nombre.done,"estoy dentro de la tarea a eliminar")}
                 <span className={`"" + ${nombre.done ? " tachado" : ""}`}>{nombre.label}</span>
                 <span className="justify-content-between"><i className="bi bi-trash miTrash" onClick={eliminarTarea}></i></span>
             </li>
--- src/js/component/tareas.jsx
+++ src/js/component/tareas.jsx
@@ -3,20 +3,27 @@ import Tarea from "./tarea.jsx";
 
 const Tareas = () => {
   const [nombreTarea, setNombreTarea] = useState([]);
-  const [subido, setSubido] = useState(false);
+  let tareasHechas = 0;
+  let direccion;
 
   const agregarTarea = (e) => {
     if (e.target.value.trim() && e.key === "Enter") {
-      setNombreTarea([...nombreTarea,{label: e.target.value, done: false}]);
-      
+      setNombreTarea([...nombreTarea, { label: e.target.value, done: false }]);
       e.target.value = "";
     }
   };
 
-  const actualizarServidorTareas = ()=>{
+  const actualizarServidorTareas = () => {
     sendTareas(nombreTarea);
-  }
-/* No logro hacer que ande el crear usuario con un arreglo vacio
+  };
+
+  const eliminarTarea = (indice) => {
+    let nombreTareaABorrar = [...nombreTarea];
+    nombreTareaABorrar[indice].done = !nombreTareaABorrar[indice].done;
+    setNombreTarea(nombreTareaABorrar);
+  };
+
+  /* No logro hacer que ande el crear usuario con un arreglo vacio
   const agregarNuevoUsuario = (user)=>{
       fetch("https://assets.breatheco.de/apis/fake/todos/user/" + user.trim(),{  
         method: "POST",
@@ -30,7 +37,7 @@ const Tareas = () => {
         .catch(error => {console.log(error)})
   }*/
 
-  const borrarUsuario = ()=>{
+  const borrarUsuario = () => {
     fetch("https://assets.breatheco.de/apis/fake/todos/user/kq", {
       method: "DELETE",
       headers: {
@@ -45,50 +52,41 @@ const Tareas = () => {
       })
       .then((data) => {
         //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-        console.log(data , "estamos en el data del sendTareas"); //esto imprimirá en la consola el objeto exacto recibido del servidor
+        console.log(data, "estamos en el data del borrarUsuario"); //esto imprimirá en la consola el objeto exacto recibido del servidor
       })
       .catch((error) => {
         //manejo de errores
         console.log(error);
       });
-
-  }
-
-  const eliminarTarea = (indice) => {
-    let nombreTareaABorrar = [...nombreTarea];
-    nombreTareaABorrar[indice].done = !nombreTareaABorrar[indice].done;
-    setNombreTarea(nombreTareaABorrar)
   };
-  
-  /*useEffect(()=>{
-      newUser()
-  },[])*/
 
-  function newUser(){
-    fetch("https://assets.breatheco.de/apis/fake/todos/user/kq", {
-      method: "POST",
-      body: JSON.stringify([{label: "Primera Tarea", done:false}]),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((resp) => {
-        console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
-        console.log(resp.status); // el código de estado = 200 o código = 400 etc.
-        //console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
-        return resp;//.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+  function newUser(direccion) {
+      console.log(direccion)
+      fetch(direccion, {
+        method: "POST",
+        body: JSON.stringify([{ label: "Primera Tarea", done: false },{ label:"Segunda Tarea", done:false}]),
+        headers: {
+          "Content-Type": "application/json",
+        },
       })
-      .then((data) => {
-        //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-        console.log(data, "estamos en el data"); //esto imprimirá en la consola el objeto exacto recibido del servidor
-      })
-      .catch((error) => {
-        //manejo de errores
-        console.log(error);
-      });
+        .then((resp) => {
+          console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+          console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+          //console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
+          return resp.json();// (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+        })
+        .then((data) => {
+          //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+          console.log(data, "estamos en el data de newUser"); //esto imprimirá en la consola el objeto exacto recibido del servidor
+        })
+        .catch((error) => {
+          //manejo de errores
+          console.log(error);
+        });
   }
 
-  function getTareas(){
+  function getTareas() {
+    console.log()
     fetch("https://assets.breatheco.de/apis/fake/todos/user/kq", {
       method: "GET",
       headers: {
@@ -99,12 +97,12 @@ const Tareas = () => {
         //console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
         //console.log(resp.status); // el código de estado = 200 o código = 400 etc.
         //console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
-        return resp.json();//.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+        return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
       })
       .then((data) => {
         //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
         setNombreTarea(data);
-        console.log("estamos en el data del getTareas"); //esto imprimirá en la consola el objeto exacto recibido del servidor
+        //console.log(data,"estamos en el data del getTareas"); //esto imprimirá en la consola el objeto exacto recibido del servidor
       })
       .catch((error) => {
         //manejo de errores
@@ -128,12 +126,24 @@ const Tareas = () => {
       })
       .then((data) => {
         //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-        console.log(data , "estamos en el data del sendTareas"); //esto imprimirá en la consola el objeto exacto recibido del servidor
+        console.log(data, "estamos en el data del sendTareas"); //esto imprimirá en la consola el objeto exacto recibido del servidor
       })
       .catch((error) => {
         //manejo de errores
         console.log(error);
       });
+  }
+  //contarTareas me da la cantidad de tareas que estan finalizadas, para poder mostrar las tareas que restan adecuadamente
+  const contarTareas = ()=>{
+    let count = 0;
+    if (Array.isArray(nombreTarea)){
+      nombreTarea.forEach((elemento)=>{
+        if (elemento.done) 
+          count++;
+          //console.log(elemento)
+        })
+    }
+    return count;
   }
 
   return (
@@ -141,35 +151,58 @@ const Tareas = () => {
       <div className="row miInput my-3">
         <input
           type="text"
-          onKeyDown={(e)=>agregarTarea(e)}
+          onKeyDown={(e) => agregarTarea(e)}
           className="col-12"
           placeholder="Ingrese la tarea a realizar"
         />
       </div>
 
       <ul className="list-group list-group-flush">
-        {(nombreTarea && nombreTarea.length > 0 ) ? (nombreTarea.map((element, index) => (
-          <Tarea
-            key={index}
-            nombre={element}
-            clase="list-group-item misTareas d-flex justify-content-between"
-            eliminarTarea={()=>eliminarTarea(index)}
-          />
-        ))) : null}
+        {(nombreTarea && nombreTarea.length > 0)
+          ? nombreTarea.map((element, index) => (
+              <Tarea
+                key={index}
+                nombre={element}
+                clase="list-group-item misTareas d-flex justify-content-between"
+                eliminarTarea={() => eliminarTarea(index)}
+              />
+            ))
+          : null}
         <li className="list-group-item text-center">
-          {nombreTarea.length === 0
+          {tareasHechas = contarTareas()}
+          {console.log(tareasHechas)}
+          {console.log(nombreTarea.length)}
+          {(tareasHechas === 0)
             ? `No hay Tareas`
-            : `cantidad de tareas de hoy : ${nombreTarea.length}`}
+            : `cantidad de tareas de hoy : ${nombreTarea.length-tareasHechas}`}
         </li>
       </ul>
       <div className="mt-3">
-        <button className="btn btn-secondary me-3" onClick={()=>getTareas()}>Recuperar tareas</button>
-        <button className="btn btn-success me-3" onClick={()=>actualizarServidorTareas()}>Guardar tareas</button>
-        <button className="btn btn-danger me-3" onClick={()=>borrarUsuario()}>Borrar usuario</button>
+        <button className="btn btn-secondary me-3" onClick={() => getTareas(direccion)}>
+          Recuperar tareas
+        </button>
+        <button className="btn btn-success me-3" onClick={() => actualizarServidorTareas(direccion)}>
+          Guardar tareas
+        </button>
+        <button className="btn btn-danger me-3" onClick={() => borrarUsuario(direccion)}>
+          Borrar usuario
+        </button>
       </div>
       <div className="mt-3">
-        <input type="text" id="miInput"/>
-        <button className="btn btn-warning mx-3" onClick={()=>newUser(document.querySelector("#miInput").value)}>Dar de alta al Usuario</button>
+        <input type="text" id="miInput" />
+        <button
+          className="btn btn-warning mx-3"
+          onClick={() => {
+            if (document.querySelector("#miInput").value.trim()) {
+              direccion = "https://assets.breatheco.de/apis/fake/todos/user/" + document.querySelector("#miInput").value.trim();
+              newUser(direccion);
+              document.querySelector("#miInput").value = "";
+            }
+            else alert("No puede crear un usuario vacio!")
+          }}
+        >
+          Dar de alta al Usuario
+        </button>
       </div>
     </div>
   );
